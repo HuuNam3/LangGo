2070c8ef39db4801ae6f1a192d15255e7b746ad3
NguyenHuuNam3 <nhnam4411@gmail.com>
2025-05-29 15:04:02 +0700

    fix css login

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,17 +1,13 @@
 "use client"
-import { Footer } from '@/components/common/Footer'
-import { Header } from '@/components/common/Header'
 import LoginPage from '@/components/common/Login'
 import React, { Suspense } from 'react'
 
 export default function Page() {
   return (
     <>
-      <Header/>
         <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
           <LoginPage/>
         </Suspense>
-      <Footer/>
     </>
   )
 }
