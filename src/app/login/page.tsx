8b76209992943ef0ba40ec575706d8f64732ba2a
NguyenHuuNam3 <nhnam4411@gmail.com>
2025-06-01 15:56:43 +0700

    edit login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 import LoginPage from '@/components/common/Login'
 import React, { Suspense } from 'react'
+import { Header } from "@/components/common/Header"
+import { Footer } from "@/components/common/Footer"
 
 export default function Page() {
   return (
     <>
+      <Header/>
         <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
           <LoginPage/>
         </Suspense>
+      <Footer/>
     </>
   )
 }
